92eadbe90623324edecd19caad89e608f6712ce1
Fernando Belizario <belizariof22@gmail.com>
2026-02-16 08:36:06 -0500

    Requisitos Añadidos

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name="Guardian Digital",
+    version="0.1.0",
+    author="fernando and fabi",
+    author_email="belizariof22@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
